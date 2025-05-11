c0b072b7b5ba17a4a1f7cff3522871bc4e83f6c9
Kawtar Choubari <choubari.kawtar@gmail.com>
2025-05-11 18:42:25 +0200

    suspense boundary

--- app/testimonials/publish/page.tsx
+++ app/testimonials/publish/page.tsx
@@ -1,7 +1,18 @@
 import PublishTestimonialClient from "./client";
+import { Suspense } from "react";
 
 export default function PublishTestimonialPage() {
   const adminUsername = process.env.GITHUB_ADMIN_USERNAME || "choubari";
 
-  return <PublishTestimonialClient adminUsername={adminUsername} />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          Loading...
+        </div>
+      }
+    >
+      <PublishTestimonialClient adminUsername={adminUsername} />
+    </Suspense>
+  );
 }
